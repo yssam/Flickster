e65de62cacc089f01d69a8a3b0543a944d9923ae
Yu-Shiuan Chu <mz0401sam.ee96@gmail.com>
2016-07-19 21:06:38 -0700

    Basic video tutorial

---
+++ app/src/main/java/com/example/sam/flickster/models/Movie.java
@@ -0,0 +1,48 @@
+package com.example.sam.flickster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sam on 7/18/16.
+ */
+public class Movie {
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    String posterPath;
+    String originalTitle;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException{
+        this.posterPath = jsonObject.getString("poster_path");
+        this.originalTitle = jsonObject.getString("original_title");
+        this.overview = jsonObject.getString("overview");
+    }
+
+    public static ArrayList<Movie> fromJSONArray(JSONArray array){
+        ArrayList<Movie> results = new ArrayList<>();
+
+        for(int x=0; x<array.length(); ++x){
+            try {
+                results.add(new Movie(array.getJSONObject(x)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return results;
+    }
+
+}
